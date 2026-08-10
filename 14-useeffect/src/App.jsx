fb6133a7073442309c48fcd201cfa1263c4e62a4
Priyanshu <pickario2356@gmail.com>
2026-08-10 22:53:49 +0530

    Started learning useEffect, thought it was difficult at first but it wasn't as difficult as I thought

--- 14-useeffect/src/App.jsx
+++ 14-useeffect/src/App.jsx
@@ -1,121 +1,45 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
-import './App.css'
+import { useEffect, useState } from "react"
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+
+  // const [num1, setNum1] = useState(0)
+  // const [num2, setNum2] = useState(100)
+  // useEffect(()=>{console.log("yohoho")})  // here without dependency array it runs after every render 
+  //useeffect(()=>{},[]) runs once only after first render
+
+  // useEffect(()=>{console.log("yohoho")},[num2])// now num2 is dependency so when it changes its run 
+
+  const [a, setA] = useState(0)
+  const [b, setB] = useState(0)
+
+function changeA(){
+  console.log("A is changed")
+}
+function changeB(){
+  console.log("B is changed")
+}
+  // useEffect(()=>{console.log("its running")},[a])
+
+ useEffect(()=>{//  Here, useEffect runs whenever [a] changes, and then the function inside {} runs, right
+  changeA()
+ },[a])// b is also changing but we didnt add b in use effect
 
   return (
-    <>
-      <section id="center">
-        <div className="hero">
-          <img src={heroImg} className="base" width="170" height="179" alt="" />
-          <img src={reactLogo} className="framework" alt="React logo" />
-          <img src={viteLogo} className="vite" alt="Vite logo" />
-        </div>
-        <div>
-          <h1>Get started</h1>
-          <p>
-            Edit <code>src/App.jsx</code> and save to test <code>HMR</code>
-          </p>
-        </div>
-        <button
-          type="button"
-          className="counter"
-          onClick={() => setCount((count) => count + 1)}
-        >
-          Count is {count}
-        </button>
-      </section>
+    <div>
+      {/* <h1>Num1 {num1} </h1><h1>Num2 {num2} </h1>
+      <button onMouseEnter={()=>{setNum1(num1+1)}}  onMouseLeave={()=>{setNum2(num2+10)}}>Click</button> */}
+    
+    <h1>A is {a}</h1>
+    <h1>B is {b}</h1>
+    
+    <button onClick={()=>
+      setA(a+1)
+    }>Change A</button>
 
-      <div className="ticks"></div>
-
-      <section id="next-steps">
-        <div id="docs">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#documentation-icon"></use>
-          </svg>
-          <h2>Documentation</h2>
-          <p>Your questions, answered</p>
-          <ul>
-            <li>
-              <a href="https://vite.dev/" target="_blank">
-                <img className="logo" src={viteLogo} alt="" />
-                Explore Vite
-              </a>
-            </li>
-            <li>
-              <a href="https://react.dev/" target="_blank">
-                <img className="button-icon" src={reactLogo} alt="" />
-                Learn more
-              </a>
-            </li>
-          </ul>
-        </div>
-        <div id="social">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#social-icon"></use>
-          </svg>
-          <h2>Connect with us</h2>
-          <p>Join the Vite community</p>
-          <ul>
-            <li>
-              <a href="https://github.com/vitejs/vite" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#github-icon"></use>
-                </svg>
-                GitHub
-              </a>
-            </li>
-            <li>
-              <a href="https://chat.vite.dev/" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#discord-icon"></use>
-                </svg>
-                Discord
-              </a>
-            </li>
-            <li>
-              <a href="https://x.com/vite_js" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#x-icon"></use>
-                </svg>
-                X.com
-              </a>
-            </li>
-            <li>
-              <a href="https://bsky.app/profile/vite.dev" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#bluesky-icon"></use>
-                </svg>
-                Bluesky
-              </a>
-            </li>
-          </ul>
-        </div>
-      </section>
-
-      <div className="ticks"></div>
-      <section id="spacer"></section>
-    </>
+    <button onClick={()=>{
+      setB(b+1)
+    }}>Change B</button>
+    </div>
   )
 }
 
